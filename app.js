d78d5900cc888c64c5dd0d631b1f0596f823b5f0
janwa039 <jaibom2@gmail.com>
2020-11-08 19:53:47 -0500

    Fixed the issue with the fon t-awesome icons not displaying

--- app.js
+++ app.js
@@ -7,44 +7,49 @@ const app = express()
 
 app.use(express.static('public'))
 
+// app.use((req, res) => {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+// });
+
 
 app.use(bodyParser.urlencoded({extended: true}))
 
-app.post('/formPage', (req, res) => {
+// app.post('/formPage', (req, res) => {
 
-    const user = process.env.GMAIL_USER
-    const pass = process.env.GMAIL_PASS  
-    // Instantiate the SMTP server
-    const smtpTrans = nodemailer.createTransport({
-        host: 'smtp.gmail.com',
-        port: 465,
-        secure: true,
-        auth: {
-            user,
-            pass
-        }
-    })
-
-
-    const mailOpts = {
-        from: 'Your sender info here', // This is ignored by Gmail
-        to: user,
-        subject: 'New message from contact form at affiongbassey.com',
-        text: `${req.body.name} (${req.body.email}) says: ${req.body.message}`
-    }
+//     const user = process.env.GMAIL_USER
+//     const pass = process.env.GMAIL_PASS  
+//     // Instantiate the SMTP server
+//     const smtpTrans = nodemailer.createTransport({
+//         host: 'smtp.gmail.com',
+//         port: 465,
+//         secure: true,
+//         auth: {
+//             user,
+//             pass
+//         }
+//     })
 
 
-      // Attempt to send the email
-    smtpTrans.sendMail(mailOpts, (error, response) => {
-        if (error) {
-            res.render('contact-failure') // Show a page indicating failure
-        }
-        else {
-            console.log('successfully sent the message', response)
-            res.render('contact-success') // Show a page indicating success
-        }
-    })
-})
+//     const mailOpts = {
+//         from: 'Your sender info here', // This is ignored by Gmail
+//         to: user,
+//         subject: 'New message from contact form at affiongbassey.com',
+//         text: `${req.body.name} (${req.body.email}) says: ${req.body.message}`
+//     }
+
+
+//       // Attempt to send the email
+//     smtpTrans.sendMail(mailOpts, (error, response) => {
+//         if (error) {
+//             res.render('contact-failure') // Show a page indicating failure
+//         }
+//         else {
+//             console.log('successfully sent the message', response)
+//             res.render('contact-success') // Show a page indicating success
+//         }
+//     })
+// })
 
 
 app.listen(process.env.PORT || 3002, ()=>{
