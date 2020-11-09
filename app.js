c830f53a4e53342aa5d3669af7682c998709d5d2
janwa039 <jaibom2@gmail.com>
2020-11-08 23:57:18 -0500

    Finished creating the contact request form

--- app.js
+++ app.js
@@ -5,11 +5,11 @@ const bodyParser = require('body-parser')
 
 const app = express()
 
-// app.use(function(req, res, next) {
-//     res.header("Access-Control-Allow-Origin", "*");
-//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//     next();
-// });
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 app.use(express.json());
 
